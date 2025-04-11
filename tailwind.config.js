be5c82bdb100a34715354015eeaa11d0627cb84f
Rakeshvimal04 <vimalrakesh2003@gmail.com>
2025-04-11 10:13:08 +0530

    view files added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    darkMode: 'class', // Use 'class' strategy for dark mode
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    darkMode:'class',
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
